ce64abea186467bce82522747d089432dbbf6f76
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-05-12 21:49:58 -0500

    Metodo validar longitud

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,13 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada):
+        self._longitud_esperada = longitud_esperada
+
+    def _validar_longitud(self, clave):
+        if len(clave) <= self._longitud_esperada:
+            raise ValueError(f"La clave debe tener más de {self._longitud_esperada} caracteres")
+
+    @abstractmethod
+    def es_valida(self, clave):
+        pass
