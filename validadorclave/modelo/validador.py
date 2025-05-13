07d03fe5e3653120cf23f3da74da4ae02d808173
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-05-12 22:06:22 -0500

    Metodo contiene mayusculas

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -8,6 +8,15 @@ class ReglaValidacion(ABC):
         if len(clave) <= self._longitud_esperada:
             raise ValueError(f"La clave debe tener más de {self._longitud_esperada} caracteres")
 
+    def _contiene_mayuscula(self, clave):
+        tiene_mayuscula = False
+        for c in clave:
+            if c.isupper():
+                tiene_mayuscula = True
+                break
+        if not tiene_mayuscula:
+            raise ValueError("La clave debe contener al menos una letra mayúscula")
+
     @abstractmethod
     def es_valida(self, clave):
         pass
