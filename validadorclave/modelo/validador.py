d66252fda8d1a7bc1a0f5b65878ce2c01fb1b7f0
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-05-12 23:01:12 -0500

    clase validador

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -87,3 +87,10 @@ class ReglaValidacionCalisto(ReglaValidacion):
         self._contiene_calisto(clave)
         return True
 
+class Validador:
+    def __init__(self, regla):
+        self.regla = regla
+    def es_valida(self, clave):
+        return self.regla.es_valida(clave)
+
+
